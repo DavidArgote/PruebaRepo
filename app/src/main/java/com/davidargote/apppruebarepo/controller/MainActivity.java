21c014dfbad9b44e309365d09782f20422457eb1
David <argote.2075@gmail.com>
2019-10-03 09:55:54 -0500

    implementación de botones

--- app/src/main/java/com/davidargote/apppruebarepo/controller/MainActivity.java
+++ app/src/main/java/com/davidargote/apppruebarepo/controller/MainActivity.java
@@ -3,14 +3,44 @@ package com.davidargote.apppruebarepo.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.davidargote.apppruebarepo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnDavid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+
+        btnDavid = findViewById(R.id.btnDavid);
+        btnDavid.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnDavid:
+                Toast.makeText(this, "Boton David", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+
     }
 }
